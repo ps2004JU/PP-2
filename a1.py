af8197d701704445d5d73a6661ca1a91a8e4a6d6
ps2004JU <punya.22bcan062@jecrcu.edu.in>
2025-03-11 15:47:44 +0530

    Second commit

--- a1.py
+++ a1.py
@@ -1,7 +1,6 @@
 import pickle as p
 a = open("D1.txt", "a+")
 a11=input("Enter something inside the file :: ")
-a.writelines(a11,end="\n")
+a.write(f"{a11}\n")
 print("Success")
-print(a.read())
 a.close()
