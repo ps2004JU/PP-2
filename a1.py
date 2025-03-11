10f8d1e4877db00b4598a8d772b12539c26be4c4
ps2004JU <punya.22bcan062@jecrcu.edu.in>
2025-03-11 16:25:49 +0530

    XXX

--- a1.py
+++ a1.py
@@ -1,6 +1,14 @@
 import pickle as p
-a = open("D1.txt", "a+")
-a11=input("Enter something inside the file :: ")
-a.write(f"{a11}\n")
-print("Success")
-a.close()
+a = open("D2.txt", "a+")
+ind = 0
+while True:
+    if ind > 0:
+        b=input("Want to exit, click on X")
+        if(b=="X" or b=="x"):
+            break
+    a11=input("Enter Name of person :: ")        
+    a.write(f"Name :: {a11}  ")
+    a12=input("Enter degree of the person :: ")  
+    a.write(f"Degree :: {a12}  ")  
+print("Thank you")    
+a.close()     
