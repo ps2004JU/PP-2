c628f1fd58cbb3dc4acf57ba82416925e29086f4
ps2004JU <punya.22bcan062@jecrcu.edu.in>
2025-03-11 17:00:57 +0530

    MAIN COMMIT

--- a1.py
+++ a1.py
@@ -1,17 +1,21 @@
 import pickle as p
 a = open("D2.txt", "a+")
-ind = 0
+b = open("D2.pkl","ab")
+ind=0
 while True:
     if ind > 0:
         b=input("Want to exit, click on X")
         if(b=="X" or b=="x"):
             break
-    a11=input("Enter Name of person :: ")        
-    a.write(f"Name :: {a11}  ")
+    ax=input("Enter Name of person :: ")        
+    a.write(f"Name :: {ax}  ")
+    p.dump(f"Name :: {ax} ",b)
     a12=input("Enter degree of the person :: ")  
     a.write(f"Degree :: {a12}  ")
+    p.dump(f"Degree:: {a12} ",b)
     a13=input("Enter Alma Mater of the person :: ")  
     a.write(f"Alma Mater :: {a13}\n")
+    p.dump(f"Alma Mater :: {a13}\n",b)
     ind+=1        
 print("Thank you")    
 a.close()     
